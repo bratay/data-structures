5ccda535c3617bebeb0ab67e1789ed31488290e5
bratay <violinbt2@yahoo.com>
2019-10-22 23:13:56 -0500

    Heap empty shell

---
+++ minmax-heap/minMaxHeap.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include "minMaxHeap.h"
+
+using namespace std;
+
+MinMaxHeap::MinMaxHeap()
+{
+    for (int i = 0; i < 800; i++)
+        heap[i] = INT_MAX;
+}
+
+bool MinMaxHeap::isEmpty()
+{
+}
+
+void MinMxHeap::isMax(int val) {}
+
+void MinMxHeap::buildHeap(int i) {}
+
+void MinMxHeap::insert(int x) {}
+
+void MinMxHeap::remove(int x) {}
+
+void MinMxHeap::deleteMin() {}
+
+void MinMxHeap::deleteMax() {}
+
+void MinMxHeap::levelorder() {}
---
+++ minmax-heap/minMaxHeap.h
@@ -0,0 +1,36 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <iostream>
+#include <stdlib.h>
+#include <limits.h>
+
+using namespace std;
+
+class MinMaxHeap {
+
+    private:
+	void insert(int x, int index, int parent);
+	int heap[800];
+
+	public:
+    MinMaxHeap();
+
+	bool isEmpty();
+
+	void isMax(int val);
+
+	void buildHeap(int i);
+
+	void insert(int x);
+
+	void remove(int x);
+
+	void deleteMin();
+
+	void deleteMax();
+
+	void levelorder();
+
+};
+#endif
