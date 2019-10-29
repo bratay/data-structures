cb2a1eff65a1f0ed3692cd27efe00a05262a55e4
bratay <violinbt2@yahoo.com>
2019-10-29 15:23:37 -0500

    Implementation of timer

--- 3-heap/executive.cpp
+++ 3-heap/executive.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdio>
+#include <ctime>
 
 using namespace std;
 
@@ -39,75 +41,90 @@ void Executive::run()
          << "6. Time_LowestPQ\n"
          << "7. Exit\n";
     cout << ": ";
-    std::cin >> choice;
+    cin >> choice;
 
     int num;
-    double time = 0;
+    clock_t start;
+    double duration;
     switch (choice)
     {
     case 1: //Insert
-      cout<<"Enter a number to add: ";
-      cin>>num;
+      cout << "Enter a number to add: ";
+      cin >> num;
 
-      if(minHeap->contains(num)){
-        cout<<num<<" already in min heap\n";
-      }else{
+      if (minHeap->contains(num))
+      {
+        cout << num << " already in min heap\n";
+      }
+      else
+      {
         minHeap->insert(num);
-        cout<<num<<" was inserted in min heap successfully\n";
+        cout << num << " was inserted in min heap successfully\n";
       }
 
-      if(maxHeap->contains(num)){
-        cout<<num<<" already in max heap\n";
-      }else{
+      if (maxHeap->contains(num))
+      {
+        cout << num << " already in max heap\n";
+      }
+      else
+      {
         maxHeap->insert(num);
-        cout<<num<<" was inserted in max Heap successfully\n";
+        cout << num << " was inserted in max Heap successfully\n";
       }
 
       break;
     case 2: //Delete
       num = minHeap->removeMin();
-      if(num == -1 ){
-        cout<<"Min heap size is 0";
-      }else{
-        cout<<num<<" delete from min heap successfully\n";
+      if (num == -1)
+      {
+        cout << "Min heap size is 0";
+      }
+      else
+      {
+        cout << num << " delete from min heap successfully\n";
       }
 
       num = maxHeap->removeMax();
-      if(num == -1 ){
-        cout<<"Max heap size is 0";
-      }else{
-        cout<<num<<" delete from max heap successfully\n";
+      if (num == -1)
+      {
+        cout << "Max heap size is 0";
+      }
+      else
+      {
+        cout << num << " delete from max heap successfully\n";
       }
       break;
     case 3: //pq high
       num = minHeap->findMin();
-      std::cout << "MinHeap min value: " << num << std::endl;
+      cout << "MinHeap min value: " << num << endl;
       num = maxHeap->findMin();
-      std::cout << "MaxHeap max value: " << num << std::endl;
+      cout << "MaxHeap max value: " << num << endl;
       break;
     case 4: //pq low
       num = minHeap->findMax();
-      std::cout << "MinHeap min value: " << num << std::endl;
+      cout << "MinHeap min value: " << num << endl;
       num = maxHeap->findMax();
-      std::cout << "MaxHeap max value: " << num << std::endl;
+      cout << "MaxHeap max value: " << num << endl;
       break;
     case 5: //level order
-      std::cout << "MinHeap levelorder\n";
+      cout << "MinHeap levelorder\n";
       minHeap->levelorder();
-      std::cout << "MaxHeap levelorder\n";
+      cout << "MaxHeap levelorder\n";
       maxHeap->levelorder();
       break;
     case 6: //time lowest
-      //start time
-      num = minHeap->findMax();
-      //end time
-      cout<<time<<" s\n";
+      start = clock();
+
+      minHeap->findMax();
+
+      duration = (clock() - start) / (double)CLOCKS_PER_SEC;
+      cout << duration << " sec\n";
       break;
     case 7: //exit
-      std::cout << "Exiting...\n";
+      cout << "Exiting...\n";
       break;
     default:
-      std::cout << "Invalid choice\n";
+      cout << "Invalid choice\n";
     }
   } while (choice != 7);
 }
