c02033ddecc4fd2622a372968e55b893eb09b08e
bratay <violinbt2@yahoo.com>
2020-02-12 18:17:14 -0600

    Algo logic

--- gerchberg–saxton-algorithm/GSAlgo.py
+++ gerchberg–saxton-algorithm/GSAlgo.py
@@ -1,21 +1,40 @@
+# import sys
+# fileName = str(sys.argv)[1]
 
 data = open("test-inputs/input_size_3.txt","r")
 data.close()
 
-s = {}
+numOfCouples = 0
 
-while( (a man is free) and (has not proposed to every woman) ):
-    man = choose a man
+s = [] #matrix with married couples
+
+mp = [] # men pref matrix
+wp = [] #women pref matrix
+wr = [] # women rank matrix
+
+singleMen = [] #get men pref matrix
+numProposed = [] #how many times have they proposed
+
+while( len(singleMen) != 0 and numProposed[singleMen[0]] <  numOfCouples):
+    man = singleMen.pop(0)
     
-    w = highest ranked woman in the man preference that he hasnt proposed to
+    # highest ranked woman in the man preference that he hasnt proposed to
+    w = mp[man][ numProposed[man] ]
+    numProposed[man] += numProposed[man] + 1
     
-    if( w is free ):
-        getEngaged(man, w)
+    if( s[w][0] == -1):
+        # getEngaged(man, w)
+        s[w] = [man, w]
     else: # w is engaged to m-
-        if( w prefers m- over man):
-            man stays free
+        m- = s[w][0] #get current husband
+        
+        if( wr[ m- ] > wr[ man ] ): # w prefers m- over man
+            # man stays free
+            singleMen.append(man)
         else: # w prefers man to m-
-            getEngaged(man, w)
-            unengaged( m- )
+            # getEngaged(man, w)
+            s[w] = [man, w]
+            # unengaged( m- )
+            s[m-] = [-1, -1]
             
 return s
