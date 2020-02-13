a152b8b7763320c0c89b99eb8b0600cb11a2de87
bratay <violinbt2@yahoo.com>
2020-02-12 18:58:25 -0600

    Initialized data structures

--- gerchberg–saxton-algorithm/GSAlgo.py
+++ gerchberg–saxton-algorithm/GSAlgo.py
@@ -1,40 +1,61 @@
-# import sys
-# fileName = str(sys.argv)[1]
+import numpy as np
+import sys
+fileName = "test-inputs/input_size_3.txt"  # str(sys.argv)[1]
 
-data = open("test-inputs/input_size_3.txt","r")
+# data = open("test-inputs/input_size_3.txt","r")
+data = open(fileName, "r")
+
+n = int(data.readline())
+
+temp = []
+for x in data:
+    temp.append(x.rstrip())
+
+del temp[0]
+del temp[n]
+
+s = []  # matrix with married couples
+mp = []  # men pref matrix
+wp = []  # women pref matrix
+wr = []  # women rank matrix
+
+singleMen = []  # get men pref matrix
+numProposed = []  # how many times have they proposed
+
+for i in range(0, n):
+    row = temp[i]
+    s.append( [ -1, -1] )
+    singleMen.append(i)
+    numProposed.append(0)
+    mp.append(row.split(','))
+    
+for i in range(n, n+n):
+    row = temp[i]
+    wp.append(row.split(','))
+
+
 data.close()
 
-numOfCouples = 0
-
-s = [] #matrix with married couples
-
-mp = [] # men pref matrix
-wp = [] #women pref matrix
-wr = [] # women rank matrix
-
-singleMen = [] #get men pref matrix
-numProposed = [] #how many times have they proposed
-
-while( len(singleMen) != 0 and numProposed[singleMen[0]] <  numOfCouples):
+while( len(singleMen) != 0 and numProposed[singleMen[0]] <  n):
     man = singleMen.pop(0)
-    
+
     # highest ranked woman in the man preference that he hasnt proposed to
     w = mp[man][ numProposed[man] ]
     numProposed[man] += numProposed[man] + 1
-    
+
     if( s[w][0] == -1):
         # getEngaged(man, w)
         s[w] = [man, w]
-    else: # w is engaged to m-
-        m- = s[w][0] #get current husband
-        
-        if( wr[ m- ] > wr[ man ] ): # w prefers m- over man
+    else: # w is engaged to curMan
+        curMan = s[w][0] #get current husband
+
+        if( wr[w][ curMan ] > wr[w][ man ] ): # w prefers curMan over man
             # man stays free
             singleMen.append(man)
-        else: # w prefers man to m-
+        else: # w prefers man to curMan
             # getEngaged(man, w)
             s[w] = [man, w]
-            # unengaged( m- )
-            s[m-] = [-1, -1]
-            
-return s
+            # unengaged( curMan )
+            s[curMan] = [-1, -1]
+
+print(s)
