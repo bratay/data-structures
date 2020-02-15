0368794c89db95896a1f98da680faf44a48c4dc5
bratay <violinbt2@yahoo.com>
2020-02-14 21:10:14 -0600

    Working logic, reformatting output needed

--- gerchberg–saxton-algorithm/GSAlgo.py
+++ gerchberg–saxton-algorithm/GSAlgo.py
@@ -14,48 +14,53 @@ for x in data:
 del temp[0]
 del temp[n]
 
-s = []  # matrix with married couples
-mp = []  # men pref matrix
-wp = []  # women pref matrix
-wr = []  # women rank matrix
+s = []  # matrix with married couples # 0 base
+mp = []  # men pref matrix # 0 based but w is 1 based
+wp = []  # women pref matrix # 0 based but m is 1 based
+wr = []  # women rank matrix # 0 based but m is 1 based
 
 singleMen = []  # get men pref matrix
 numProposed = []  # how many times have they proposed
 
 for i in range(0, n):
     row = temp[i]
-    s.append( [ -1, -1] )
+    s.append(-1)
     singleMen.append(i)
     numProposed.append(0)
     mp.append(row.split(','))
-    
+    wr.append(row.split(','))
+
 for i in range(n, n+n):
     row = temp[i]
     wp.append(row.split(','))
 
+for i in range(n):
+    for j in range(n):
+        theMan = wp[i][j]
+        wr[i][int(theMan) - 1] = j
 
 data.close()
 
-while( len(singleMen) != 0 and numProposed[singleMen[0]] <  n):
-    man = singleMen.pop(0)
+while(len(singleMen) != 0 and numProposed[singleMen[0]] < n):
+    man = singleMen.pop(0)  # 0 base
 
     # highest ranked woman in the man preference that he hasnt proposed to
-    w = mp[man][ numProposed[man] ]
-    numProposed[man] += numProposed[man] + 1
+    w = int(mp[man][numProposed[man]]) - 1
+    numProposed[man] = numProposed[man] + 1
 
-    if( s[w][0] == -1):
+    if(s[w] == -1):
         # getEngaged(man, w)
-        s[w] = [man, w]
-    else: # w is engaged to curMan
-        curMan = s[w][0] #get current husband
+        s[w] = man
+    else:  # w is engaged to curMan
+        curMan = s[w]  # get current husband
 
-        if( wr[w][ curMan ] > wr[w][ man ] ): # w prefers curMan over man
+        if(wr[w][curMan] < wr[w][man]):  # w prefers curMan over man
             # man stays free
             singleMen.append(man)
-        else: # w prefers man to curMan
+        else:  # w prefers man to curMan
             # getEngaged(man, w)
-            s[w] = [man, w]
+            s[w] = man
             # unengaged( curMan )
-            s[curMan] = [-1, -1]
+            singleMen.append(curMan)
 
 print(s)
