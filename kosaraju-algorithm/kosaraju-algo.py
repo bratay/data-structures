1c6047a2bd49f9d5545cfb2a76314c5471031770
bratay <violinbt2@yahoo.com>
2020-03-06 14:57:47 -0600

    Algo outline

---
+++ kosaraju-algorithm/kosaraju-algo.py
@@ -0,0 +1,20 @@
+
+
+def dfs():
+    result = []
+    
+    
+    return result
+
+
+def findStrongComponents():
+    result = []
+    
+    
+    return result
+
+
+def initialize():
+    findStrongComponents()
+    
+    
