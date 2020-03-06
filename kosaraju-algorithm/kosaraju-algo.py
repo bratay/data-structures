574e1657ca6cab69c5c29379c1c845994340f5f1
bratay <violinbt2@yahoo.com>
2020-03-06 15:39:44 -0600

    Data structures initialized

--- kosaraju-algorithm/kosaraju-algo.py
+++ kosaraju-algorithm/kosaraju-algo.py
@@ -1,20 +1,52 @@
+# import numpy as np
+import sys
 
+graph = {} 
 
 def dfs():
     result = []
-    
-    
+
     return result
 
 
 def findStrongComponents():
     result = []
-    
-    
+
     return result
 
 
 def initialize():
+    # fileName = str(sys.argv[1])
+    fileName = "test-inputs/input_size_3.txt"
+
+    data = open(fileName, "r")
+
+    n = int(data.readline())
+
+    temp = []
+    for x in data:
+        temp.append(x.rstrip())
+
+    del temp[0]
+    # del temp[n]
+    print(str(n))
+    print(temp)
+    
+    curRow = []
+    for x in range(0, n):
+        for i in range(0 , len(temp[x]) ):
+            num = temp[x][i]
+
+            if( num != " " ):
+                curRow.append( int(num) )
+                
+        graph[ x+1 ] = curRow
+        curRow = []
+
+    data.close()
     findStrongComponents()
     
     
+    
+    
+initialize()
